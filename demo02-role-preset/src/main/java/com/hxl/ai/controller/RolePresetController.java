827a2fea9eb76f923fa78d6101ffcf7971c09556
hxl <hxl@git.com>
2025-11-30 16:22:27 +0800

    [2025/11/30]SpringAI预设角色流式响应和乱码解决

--- demo02-role-preset/src/main/java/com/hxl/ai/controller/RolePresetController.java
+++ demo02-role-preset/src/main/java/com/hxl/ai/controller/RolePresetController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * 角色预设
@@ -21,6 +22,18 @@ public class RolePresetController {
         return client.prompt() // 创建一个 PromptBuilder 对象
                 .user(msg) // 设置用户输入
                 .call() // 调用 ChatClient 对象的 call 方法
+                .content(); // 获取返回结果
+    }
+
+    /**
+     * 角色预设流式输出
+     */
+    @GetMapping(value = "/chat/stream",
+                produces = "text/html;charset=UTF-8")
+    public Flux<String> chatStream(@RequestParam("msg") String msg) {
+        return client.prompt() // 创建一个 PromptBuilder 对象
+                .user(msg) // 设置用户输入
+                .stream() // 调用 ChatClient 对象的 stream 方法
                 .content(); // 获取返回结果
     }
 }
